12de947e0f35eae3f8a98cbd17bf11c014444d5c
unknown <jhonerix23@gmail.com>
2023-10-15 22:28:26 +0800

    modified servicespage

--- src/views/ServicesPage/ServicesOffer.jsx
+++ src/views/ServicesPage/ServicesOffer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ServicesOffer() {
   return (
     <div className='justify-center items-center'>
-        <h1 className='text-center text-xSmall text-primary-green font-extrabold'>This is services section</h1>
+        <h1 className='text-center text-xSmall text-primary-green font-extrabold'>This is services</h1>
     </div>
   )
 }
