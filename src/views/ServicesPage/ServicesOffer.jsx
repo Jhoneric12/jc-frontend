64c0b55fa06e8760ff8af6f7bbefac07e080160e
unknown <jhonerix23@gmail.com>
2023-10-18 08:24:44 +0800

    modified margins at services offer

--- src/views/ServicesPage/ServicesOffer.jsx
+++ src/views/ServicesPage/ServicesOffer.jsx
@@ -11,7 +11,7 @@ function ServicesOffer() {
             <div key={services.id} className='bg-puti p-10 rounded-[8px] drop-shadow-lg flex flex-col'>
                 <img src={services.serviceImg} alt="" className='image mb-5 w-full'/>
                 <h1 className='text-xSmall font-semibold mb-2'>{services.serviceName}</h1>
-                <h1 className='text-small font-bold text-primary-green mb-2 mt-auto'>{services.price}</h1>
+                <h1 className='text-small font-bold text-primary-green mb-2'>{services.price}</h1>
                 <p className='text-xxSmall font-light mb-4 '>{services.description}</p>
                 <div className='mt-auto'>
                   <Button
