58023a511164051c2299da640416bc540e07716b
unknown <jhonerix23@gmail.com>
2023-10-20 18:04:48 +0800

    nothing

--- src/views/ServicesPage/ServicesOffer.jsx
+++ src/views/ServicesPage/ServicesOffer.jsx
@@ -11,14 +11,14 @@ function ServicesOffer() {
             <div key={services.id} className='bg-puti p-10 rounded-[8px] drop-shadow-lg flex flex-col'>
                 <img src={services.serviceImg} alt="" className='image mb-5 w-full'/>
                 <h1 className='text-xSmall font-semibold mb-2'>{services.serviceName}</h1>
-                <h1 className='text-small font-bold text-primary-green mb-2'>{services.price}</h1>
+                <h1 className='text-small font-bold text-primary-green mb-2 mt-auto'>{services.price}</h1>
                 <p className='text-xxSmall font-light mb-4 '>{services.description}</p>
                 <div className='mt-auto'>
                   <Button
                   btnText={'Book Appointment'}
                   fontColor={'text-puti'}
                   bgColor={'bg-primary-green'}
-                  width={'w-[100%]'}
+                  width={'w-[100%]'} sdflsfjslkfjslkfj
                   />
                 </div>
             </div>
