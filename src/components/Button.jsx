f1cc09395214d06904608f2b679949145946c456
unknown <jhonerix23@gmail.com>
2023-10-21 21:23:19 +0800

    modified button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button( {btnText, bgColor, borderColor, borderStyle, borderWeight, fontColor, width, borderRound, handleClick} ) {
   return (
-    <button className={`${borderColor} ${borderStyle} ${borderWeight} font-regular ${bgColor} ${fontColor} text-xxSmall py-2 px-4 md:px-4 md:py-2 lg:px-12 lg:py-3 ${width} hover:opacity-90 ${borderRound}`} onClick={handleClick}>
+    <button className={`${borderColor} ${borderStyle} ${borderWeight} font-regular ${bgColor} ${fontColor} rounded-[8px] text-xxSmall py-2 px-4 md:px-4 md:py-2 lg:px-12 lg:py-3 ${width} hover:opacity-90 ${borderRound}`} onClick={handleClick}>
         {btnText}
     </button>
   )
--- src/components/HeaderLogo.jsx
+++ src/components/HeaderLogo.jsx
@@ -3,7 +3,7 @@ import jclogo from '../assets/HomepageImg/jcslogo.png'
 
 function HeaderLogo() {
   return (
-    <img src={jclogo} alt="jc's logo" className='w-[5rem] h-[3.5rem] lg:w-[30rem] lg:h-[23rem]'/>
+    <img src={jclogo} alt="jc's logo" className='w-[6rem] h-[4.5rem] lg:w-[30rem] lg:h-[23rem]'/>
   )
 }
 
