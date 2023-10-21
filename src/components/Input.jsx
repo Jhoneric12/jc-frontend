e56e6506d81322d8488801485ddc041daa868738
unknown <jhonerix23@gmail.com>
2023-10-21 21:54:55 +0800

    modified fonts in inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ function Input({inputType, inputName, placeHolder}) {
   return (
     <div>
       <input type={inputType} name={inputName} placeholder={placeHolder} 
-      className=' text-border-color border-border-color border-2 px-4 py-2 rounded-[8px] w-full text-xxSmall focus:border-border-color focus:outline-none text-border-border-color md:text-xSmall'/>
+      className='mt-1 font-light text-border-color border-border-color border-2 px-4 py-2 rounded-[8px] w-full text-xSmall focus:border-primary-green focus:outline-none hover:border-primary-green focus:drop-shadow-md text-border-border-color'/>
     </div>
   )
 }
