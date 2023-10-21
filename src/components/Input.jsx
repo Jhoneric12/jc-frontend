ba13a09d72d2bb317be27259e14e858771b9f896
unknown <jhonerix23@gmail.com>
2023-10-21 21:34:17 +0800

    modified input and label component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ function Input({inputType, inputName, placeHolder}) {
   return (
     <div>
       <input type={inputType} name={inputName} placeholder={placeHolder} 
-      className=' text-border-color border-border-color border-2 px-4 py-2 rounded-[8px] w-full text-xSmall focus:border-border-color focus:outline-none text-border-border-color'/>
+      className=' text-border-color border-border-color border-2 px-4 py-2 rounded-[8px] w-full text-xxSmall focus:border-border-color focus:outline-none text-border-border-color md:text-xSmall'/>
     </div>
   )
 }
