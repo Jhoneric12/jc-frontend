fb2b0748032800b5291a1a1bf17b24a7f785a7bf
unknown <jhonerix23@gmail.com>
2023-10-27 18:44:51 +0800

    modified sendotp and enterotp page

--- src/views/SignUp/EnterOTP.jsx
+++ src/views/SignUp/EnterOTP.jsx
@@ -7,20 +7,20 @@ import HeaderLogo from '../../components/HeaderLogo'
 
 function EnterOTP() {
   return (
-    <section className='h-screen flex flex-col items-center justify-center py-10 px-6 lg:flex-row lg:justify-around'>
+    <section className='h-screen flex flex-col items-center justify-center py-10 px-6 lg:flex-row lg:justify-around'>   
         <div className='mb-4 md:mb-6'>
             <FormTitle formTitle={'Welcome to'} fontSize={'text-superSmall md:text-xSmall'} fontWeight={'font-regular'}/>
             <HeaderLogo/>
         </div>
         <div className='w-full md:w-1/2 lg:w-[40%]'>
+            <form action="" className='py-10 px-5 bg-ultra-puti drop-shadow-xl rounded-[8px]'>
             <div className='mb-6'>
                 <FormTitle formTitle={'Enter your One-Time Pin (OTP)'} fontWeight={'font-bold'} fontSize={'text-small'}/>
                 <p className='text-superSmall md:text-xxSmall font-light text-border-color mt-1'>We have sent the OTP in your email address</p>
             </div>
-            <form action="" className='py-10 px-5 bg-ultra-puti drop-shadow-xl rounded-[8px]'>
             <div>
                 <Label inputLabel={'Enter OTP'}/>
-                <Input placeHolder={'OTP'} inputType={'text'}/>
+                <Input placeHolder={'Enter OTP'} inputType={'text'}/>
                 <p className='text-[0.625rem] mt-2 mb-4 md:text-superSmall'>Didn't received a code?</p>
                 <p className='text-primary-green text-[0.625rem] mt-1 hover:cursor-pointer md:text-superSmall'>Resend Code</p>
                 <p className='text-primary-green text-[0.625rem] mt-1  mb-6 hover:cursor-pointer md:text-superSmall'>Use phone number instead</p>
--- src/views/SignUp/SendOTP.jsx
+++ src/views/SignUp/SendOTP.jsx
@@ -21,18 +21,18 @@ function SendOTP() {
             <HeaderLogo/>
         </div>
         <div className='w-full md:w-[60%] lg:w-[40%]'>
-            <div className='flex items-start flex-col mb-6'>
-                <FormTitle formTitle={'OTP Verification'} fontWeight={'font-bold'} fontSize={'text-small'}/>
-                 <p className='text-superSmall md:text-xxSmall font-light text-border-color mt-1'>Verify your account details.</p>
-            </div>
             <form action="" className='flex flex-col  bg-ultra-puti px-5 py-10 rounded-[8px] drop-shadow-xl'>
+                <div className='flex items-start flex-col mb-6'>
+                    <FormTitle formTitle={'OTP Verification'} fontWeight={'font-bold'} fontSize={'text-small'}/>
+                    <p className='text-superSmall md:text-xxSmall font-light text-border-color mt-1'>Verify your account details.</p>
+                </div>
                 <div className='flex justify-center mb-6'>
                     <img src={OTPLogo} alt="" />
                 </div>
                 <div>
                     <Label inputLabel={'Email Address'}/>
-                    <Input placeHolder={'Email Address'} inputType={'email'}/>
-                    <p className='text-primary-green text-[0.625rem] mt-1  mb-6 hover:cursor-pointer md:text-superSmall'>Use phone number instead</p>
+                    <Input placeHolder={'Enter Email Address'} inputType={'email'}/>
+                    <p className='text-primary-green text-[0.625rem] mt-2  mb-6 hover:cursor-pointer md:text-superSmall'>Use phone number instead</p>
                     <Button btnText={'Send OTP'} bgColor={'bg-primary-green'} width={'w-full'} fontColor={'text-puti'} handleClick={navigateEnterOTP}/>
                 </div>
             </form>
